c183c51587169106268435a55758a9faedd348a1
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2022-09-07 11:50:48 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/DsMeta/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.DsMeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.DsMeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	
+}
